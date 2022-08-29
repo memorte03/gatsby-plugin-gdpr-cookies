7aeae7f0882f4906e1f7504f463ebbc73de9493a
irekrog <irek.rog@mobilereality.pl>
2022-08-29 07:14:01 +0200

    fix: move hubspot script to end of the body

--- src/services/hubspot.js
+++ src/services/hubspot.js
@@ -4,7 +4,7 @@ exports.addHubspot = (options) => {
   return new Promise((resolve, reject) => {
     if (window.gatsbyPluginGDPRCookiesHubspotAdded) return resolve(true)
 
-    const head = document.getElementsByTagName(`head`)[0]
+    const head = document.body
     const script = document.createElement(`script`)
     script.type = `text/javascript`
     script.id = `hs-script-loader`
@@ -27,25 +27,6 @@ exports.initializeHubspot = (options) => {
     getCookie(options.cookieName) === `true` &&
     validHubspotTrackingId(options)
   ) {
-    // (function(){
-    const _hsq = (window._hsq = window._hsq || [])
-    _hsq.push([
-      `setPath`,
-      window.location.pathname + window.location.search + window.location.hash,
-    ])
-    ;`${
-      options.respectDNT
-        ? `
-                  if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
-                    if (window.doNotTrack == '1' || navigator.doNotTrack == 'yes' || navigator.doNotTrack == '1' || navigator.msDoNotTrack == '1' || window.external.msTrackingProtectionEnabled()) {
-                      _hsq.push(['doNotTrack']);
-                    }
-                  }
-                `
-        : ``
-    }`
-    // })();
-
     window.gatsbyPluginGDPRCookiesHubspotInitialized = true
   }
 }
